3cfcbc07d338836e9087f1df6b09f7f2d554faf5
zaharchenko <zaharchenko@haulmont.com>
2019-09-02 14:03:08 +0400

    CUBA 7.1.0

---
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -0,0 +1,37 @@
+-- begin YETDEMO_SUBSCRIPTION
+create table YETDEMO_SUBSCRIPTION (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255),
+    ACTIVE boolean not null,
+    EXPIRY_DATE date not null,
+    CUSTOMER_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end YETDEMO_SUBSCRIPTION
+-- begin YETDEMO_CUSTOMER
+create table YETDEMO_CUSTOMER (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255),
+    SURNAME varchar(255),
+    EMAIL varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end YETDEMO_CUSTOMER
---
+++ modules/core/db/update/postgres/19/190521-1-createCustomer.sql
@@ -0,0 +1,16 @@
+create table YETDEMO_CUSTOMER (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255),
+    SURNAME varchar(255),
+    EMAIL varchar(255) not null,
+    --
+    primary key (ID)
+);
